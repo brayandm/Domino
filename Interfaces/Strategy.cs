5875809c0a880dfc7c7893679f05143829d83aa1
brayandm <brayanduranmedina@gmail.com>
2022-06-23 22:23:18 -0400

    Strategies added

--- Interfaces/Strategy.cs
+++ Interfaces/Strategy.cs
@@ -1,4 +1,36 @@
 interface IStrategy : IBaseInterface
 {
+    int ChooseTokenIndex(List<Token> tokens);
+}
 
+class GreedyStrategy : IStrategy
+{
+    public int ChooseTokenIndex(List<Token> tokens)
+    {
+        if(tokens.Count == 0)return -1;
+     
+        Token token = tokens[0];
+        int result = 0;
+
+        for(int i = 0 ; i < tokens.Count ; i++)
+        {
+            if(token.CompareTo(tokens[i]) < 0)
+            {
+                token = tokens[i];
+                result = i;
+            }
+        }
+
+        return result;
+    }
+}
+
+class RandomStrategy : IStrategy
+{
+    public int ChooseTokenIndex(List<Token> tokens)
+    {
+        if(tokens.Count == 0)return -1;
+
+        return new Random().Next(tokens.Count);
+    }
 }
