be225aa580612972da607704e3119b2db1f92a4f
brayandm <brayanduranmedina@gmail.com>
2022-06-24 13:19:42 -0400

    PlayerInfo PlayerBoard added

--- Game/Players/PlayerInfo.cs
+++ Game/Players/PlayerInfo.cs
@@ -1,12 +1,22 @@
+using System.Diagnostics;
+
 class PlayerInfo
 {
     public List<Player> Players;
-
     public IOrderPlayer OrderPlayer;
+    public Dictionary<Player, Board> PlayerBoard;
 
-    public PlayerInfo(List<Player> players, IOrderPlayer orderPlayer)
+    public PlayerInfo(List<Player> players, List<Board> boards, IOrderPlayer orderPlayer)
     {
+        Debug.Assert(players.Count == boards.Count);
+
         this.Players = players;
         this.OrderPlayer = orderPlayer;
+        this.PlayerBoard = new Dictionary<Player, Board>();
+
+        for(int i = 0 ; i < Math.Min(players.Count, boards.Count) ; i++)
+        {
+            PlayerBoard.Add(players[i], boards[i]);
+        }
     }
 }
