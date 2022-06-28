bd18694ee32c17fb40a400f56731888f9c05a5f7
brayandm <brayanduranmedina@gmail.com>
2022-06-28 02:08:28 -0400

    Game logic updated

--- GameLogic/GameLogic.cs
+++ GameLogic/GameLogic.cs
@@ -1,9 +1,29 @@
 class GameLogic
 {
-    public GameLogic(Game game, IGraphicinterface graphicinterface)
+    public GameLogic()
     {
-        Events.MainEvent mainEvent = new Events.MainEvent();
+        IGraphicinterface graphicinterface = new ConsoleInterface();
 
-        mainEvent.Start(game, graphicinterface);
+        graphicinterface.Main();
+
+        while(true)
+        {
+            graphicinterface.NewGame();
+            
+            ITokenGenerator tokenGenerator = (ITokenGenerator)DependencyContainerRegister.Register.Organizer.GetInstanceFromDefault(typeof(ITokenGenerator));
+            IFaceGenerator faceGenerator = (IFaceGenerator)DependencyContainerRegister.Register.Organizer.GetInstanceFromDefault(typeof(IFaceGenerator));
+            IFilterTokenRules filterTokenRules = (IFilterTokenRules)DependencyContainerRegister.Register.Organizer.GetInstanceFromDefault(typeof(IFilterTokenRules));
+            IPlayerGenerator playerGenerator = (IPlayerGenerator)DependencyContainerRegister.Register.Organizer.GetInstanceFromDefault(typeof(IPlayerGenerator));
+            IOrderPlayer orderPlayer = (IOrderPlayer)DependencyContainerRegister.Register.Organizer.GetInstanceFromDefault(typeof(IOrderPlayer));
+
+            Game game = new Game(tokenGenerator, faceGenerator, filterTokenRules, playerGenerator, orderPlayer);
+
+            Events.MainEvent mainEvent = new Events.MainEvent();
+
+            mainEvent.Start(game, graphicinterface);
+
+            graphicinterface.GameOver(game);
+        }
+
     }
 }
