a37670bfd1c1bc738a7327319250696cd38ad337
brayandm <brayanduranmedina@gmail.com>
2022-06-28 02:08:39 -0400

    graphicinterface added

--- Graphicinterface/IGraphicinterface.cs
+++ Graphicinterface/IGraphicinterface.cs
@@ -1,4 +1,31 @@
 interface IGraphicinterface
 {
+    void Main();
+    void NewGame();
+    void GameOver(Game game);
     void Update(Game game);
+}
+
+class ConsoleInterface : IGraphicinterface
+{
+    public void Main()
+    {
+
+
+    }
+
+    public void NewGame()
+    {
+
+    }
+
+    public void GameOver(Game game)
+    {
+
+    }
+
+    public void Update(Game game)
+    {
+
+    }
 }
