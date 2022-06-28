177c73d940ff0f0e5acac5e5f96b80c5e9f70e29
brayandm <brayanduranmedina@gmail.com>
2022-06-28 02:36:21 -0400

    states, events and game updated

--- GameLogic/States/States.cs
+++ GameLogic/States/States.cs
@@ -1,6 +1,6 @@
 class States
 {
-    public bool IsAllPlayersPassed(Game game)
+    public static bool IsAllPlayersPassed(Game game)
     {
         if(game.GetNumberOfContiguousPassedTurns() >= game.GetNumberOfPlayers())
         {
@@ -12,7 +12,19 @@ class States
         }
     }
 
-    public bool Identity(Game game)
+    public static bool CurrentPlayerPlayedAllTokens(Game game)
+    {
+        if(game.CurrentPlayerPlayedAllTokens() == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool Identity(Game game)
     {
         return true;
     }
