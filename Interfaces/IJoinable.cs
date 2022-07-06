1e9706b55bff4eb34eb771a1547b9b158338a7c9
brayandm <brayanduranmedina@gmail.com>
2022-07-06 02:00:57 -0400

    IJoinable updated

--- Interfaces/IJoinable.cs
+++ Interfaces/IJoinable.cs
@@ -7,36 +7,46 @@ interface IJoinable : IBaseInterface, ISelector
 
 class ClassicJoinById : IJoinable
 {
-    public bool IsJoinable(Game game, ProtectedToken protectedTokenA, ProtectedToken protectedTokenB)
+    public bool IsJoinable(Game game, ProtectedToken? protectedTokenA, ProtectedToken? protectedTokenB)
     {
-        IFace faceA = protectedTokenA.GetTokenWithoutVisibility().Faces.Item2;
-        IFace faceB = protectedTokenB.GetTokenWithoutVisibility().Faces.Item1;
+        if(protectedTokenA is ProtectedToken && protectedTokenB is ProtectedToken)
+        {
+            IFace faceA = protectedTokenA.GetTokenWithoutVisibility().Faces.Item2;
+            IFace faceB = protectedTokenB.GetTokenWithoutVisibility().Faces.Item1;
 
-        return faceA.Id == faceB.Id;
+            return faceA.Id == faceB.Id;
+        }
+
+        return true;
     }
 }
 
 class JoinByIdAndDifferentTeam : IJoinable
 {
-    public bool IsJoinable(Game game, ProtectedToken protectedTokenA, ProtectedToken protectedTokenB)
+    public bool IsJoinable(Game game, ProtectedToken? protectedTokenA, ProtectedToken? protectedTokenB)
     {
-        IFace faceA = protectedTokenA.GetTokenWithoutVisibility().Faces.Item2;
-        IFace faceB = protectedTokenB.GetTokenWithoutVisibility().Faces.Item1;
-
-        Player? playerA = protectedTokenA.GetCurrentOwner();
-        Player? playerB = protectedTokenB.GetCurrentOwner();
-
-        Debug.Assert(playerA is Player);
-        Debug.Assert(playerB is Player);
-
-        if(playerA is Player && playerB is Player)
+        if(protectedTokenA is ProtectedToken && protectedTokenB is ProtectedToken)
         {
-            Team teamA = game.GetPlayerTeam(playerA);
-            Team teamB = game.GetPlayerTeam(playerB);
+            IFace faceA = protectedTokenA.GetTokenWithoutVisibility().Faces.Item2;
+            IFace faceB = protectedTokenB.GetTokenWithoutVisibility().Faces.Item1;
 
-            return faceA.Id == faceB.Id && teamA != teamB;
+            Player? playerA = protectedTokenA.GetCurrentOwner();
+            Player? playerB = protectedTokenB.GetCurrentOwner();
+
+            Debug.Assert(playerA is Player);
+            Debug.Assert(playerB is Player);
+
+            if(playerA is Player && playerB is Player)
+            {
+                Team teamA = game.GetPlayerTeam(playerA);
+                Team teamB = game.GetPlayerTeam(playerB);
+
+                return faceA.Id == faceB.Id && teamA != teamB;
+            }
+
+            return false;
         }
 
-        return false;
+        return true;
     }
 }
