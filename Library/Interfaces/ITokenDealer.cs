a9dde73244d2c024b0f008beb298f73413d3a217
brayandm <brayanduranmedina@gmail.com>
2022-07-14 04:59:49 -0400

    token dealer variable

--- Library/Interfaces/ITokenDealer.cs
+++ Library/Interfaces/ITokenDealer.cs
@@ -1,6 +1,58 @@
+using System.Diagnostics;
+
 interface ITokenDealer : IBaseInterface, ISelector
 {
     void Distribute(Box box, List<Board> boards);
+}
+
+class VariableTokensDistribution : ITokenDealer
+{
+    private int _tokensToDeal = 0;
+
+    public VariableTokensDistribution()
+    {
+        while(true)
+        {
+            Console.WriteLine("Insert the number of tokens to deal to each player (must be greater than zero):\n\n\n\n");
+            
+            string? entry = Console.ReadLine();
+
+            if(entry is string)
+            {
+                try
+                {
+                    this._tokensToDeal = int.Parse(entry);
+                    
+                    if(this._tokensToDeal <= 0)
+                    {
+                        Console.WriteLine("\n\n\n\nThe inserted number is incorrect, repeat it again\n\n");
+                        
+                        continue;
+                    }
+
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\n\n\n\nThe inserted number is incorrect, repeat it again\n\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n\n\n\nThe inserted number is incorrect, repeat it again\n\n");
+            }
+        }
+
+        Console.WriteLine("\n\n\n\n");
+    }
+
+    public void Distribute(Box box, List<Board> boards)
+    {
+        foreach(Board board in boards)
+        {
+            board.Add(box.Take(this._tokensToDeal));
+        }
+    }
 }
 
 class ClassicTenTokensDistribution : ITokenDealer
