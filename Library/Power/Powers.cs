fb741dfad5e98f659dcb776aaba88cb05b816c67
brayandm <brayanduranmedina@gmail.com>
2022-07-14 02:10:42 -0400

    powers added

---
+++ Library/Power/Powers.cs
@@ -0,0 +1,9 @@
+class PassNextPlayerTurnPower : Power
+{
+    public PassNextPlayerTurnPower() : base("PassNextPlayerTurnPower", 1, new List<bool>(){true}) {}
+}
+
+class PlayCurrentPlayerAgainPower : Power
+{
+    public PlayCurrentPlayerAgainPower() : base("PlayCurrentPlayerAgainPower", 1, new List<bool>(){true}) {}
+}
