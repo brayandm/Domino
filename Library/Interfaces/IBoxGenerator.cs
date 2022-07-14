7d6027e152bbdb8124d1cd226aaaea6a9a0384e0
brayandm <brayanduranmedina@gmail.com>
2022-07-14 05:12:25 -0400

    last change

--- Library/Interfaces/IBoxGenerator.cs
+++ Library/Interfaces/IBoxGenerator.cs
@@ -3,6 +3,67 @@ interface IBoxGenerator : IBaseInterface, ISelector
     List<ProtectedToken> Generate();
 }
 
+class VariableIntFacesBoxGenerator : IBoxGenerator
+{
+    private int _numberOfDifferentFaces = 0;
+
+    public VariableIntFacesBoxGenerator()
+    {
+        while(true)
+        {
+            Console.WriteLine("Insert the number of different faces to generate (must be greater than zero):\n\n\n\n");
+            
+            string? entry = Console.ReadLine();
+
+            if(entry is string)
+            {
+                try
+                {
+                    this._numberOfDifferentFaces = int.Parse(entry);
+                    
+                    if(this._numberOfDifferentFaces <= 0)
+                    {
+                        Console.WriteLine("\n\n\n\nThe inserted number is incorrect, repeat it again\n\n");
+                        
+                        continue;
+                    }
+
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\n\n\n\nThe inserted number is incorrect, repeat it again\n\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n\n\n\nThe inserted number is incorrect, repeat it again\n\n");
+            }
+        }
+
+        Console.WriteLine("\n\n\n\n");
+    }
+
+    public List<ProtectedToken> Generate()
+    {
+        IntFacesGenerator faceGenerator = new IntFacesGenerator();
+        ITokenGenerator tokenGenerator = new ClassicTokenGenerator();
+        IFilterTokenRule filterTokenRule = new NonFilterBoxRules();
+
+        List<Token> tokens = tokenGenerator.Generate(faceGenerator.GetFaces(_numberOfDifferentFaces));
+        tokens.RemoveAll(x => !filterTokenRule.Apply(x));
+
+        List<ProtectedToken> protectedTokens = new List<ProtectedToken>();
+        
+        foreach(Token token in tokens)
+        {
+            protectedTokens.Add(new ProtectedToken(token));
+        }
+
+        return protectedTokens;
+    }
+}
+
 class ClassicTenBoxGenerator : IBoxGenerator
 {
     public List<ProtectedToken> Generate()
