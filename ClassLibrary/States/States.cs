ef9b47e3dfdd8494446c0b2c39aad185a496d8e0
brayandm <brayanduranmedina@gmail.com>
2022-07-15 21:41:23 -0400

    states commit

--- ClassLibrary/States/States.cs
+++ ClassLibrary/States/States.cs
@@ -1,30 +1,37 @@
+// Esta clase representa una coleccion de estados
 public class States
 {
+    // Esta clase representa el estado identidad
     public static bool Identity(Game game)
     {
         return true;
     }
 
+    // Esta clase representa el estado de que todos los jugadores se pasaron
     public static bool IsAllPlayersPassed(Game game)
     {
         return game.GetNumberOfContiguousPassedTurns() >= game.GetNumberOfPlayers();
     }
 
+    // Esta clase representa el estado de que el ultimo jugador se paso
     public static bool IsLastPlayerPassed(Game game)
     {
         return game.GetNumberOfContiguousPassedTurns() != 0;
     }
 
+    // Esta clase representa el estado de que el ultimo jugador no se paso
     public static bool IsNotLastPlayerPassed(Game game)
     {
         return !IsLastPlayerPassed(game);
     }
 
+    // Esta funcion retorna si el jugador actual jugo todas sus fichas
     public static bool CurrentPlayerPlayedAllTokens(Game game)
     {
         return game.CurrentPlayerPlayedAllTokens() == true;
     }
 
+    // Esta funcion retorna si la ronda termino
     public static bool IsRoundGameOver(Game game)
     {
         if(game.PowerHandler.GetActivity("EndRoundPower") || game.PowerHandler.GetActivity("EndGamePower"))
@@ -37,6 +44,7 @@ public class States
         return roundFinalizationRule.IsRoundGameOver(game);
     }
 
+    // Esta funcion retorna el el juego termino
     public static bool IsGameOver(Game game)
     {
         if(game.PowerHandler.GetActivity("EndGamePower"))
@@ -49,6 +57,7 @@ public class States
         return gameFinalizable.IsGameFinalizable(game);
     }
 
+    // Esta funcion retorna si la condicion para invertir el orden de los jugadores se cumple
     public static bool IsConditionMetToReverse(Game game)
     {
         IReversePlayerOrder reversePlayerOrder = (IReversePlayerOrder)DependencyContainerRegister.Getter.GetInstance(typeof(IReversePlayerOrder));
@@ -56,6 +65,7 @@ public class States
         return reversePlayerOrder.IsConditionMet(game);
     }
 
+    // Esta funcion retorna si se puede robar de la caja
     public static bool IsDrawable(Game game)
     {
         IDrawable drawable = (IDrawable)DependencyContainerRegister.Getter.GetInstance(typeof(IDrawable));
